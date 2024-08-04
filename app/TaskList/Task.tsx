eda1958eddcb8bf8c3a6fc683905a5cc2e9c6ce8
VanKiet <vankietsvk@gmail.com>
2024-08-04 18:26:53 +0700

    tt
    tt

--- app/TaskList/Task.tsx
+++ app/TaskList/Task.tsx
@@ -1,18 +1,22 @@
-import { useState } from "react";
+// components/TaskItem.js
+import React from "react";
 
-export default function TaskItem(
-  // params: { n: string }
-  //   { n }: { n: string }
-  { taskName, Des }: { taskName: string; Des: string }
-) {
-  const [done, setDone] = useState(false);
-
+const TaskItem = () => {
   return (
-    <div className="flex flex-row items-stretch">
-      <div className={done ? "line-through" : ""}>{taskName} {Des}</div>
-      <button className="p-1 bg-green-400" onClick={() => setDone(true)}>
-        V
-      </button>
+    <div className="flex items-center p-4 bg-white shadow rounded-md mb-4">
+      <div className="w-4 h-4 border-2 border-blue-500 rounded-full flex-shrink-0"></div>
+      <div className="ml-4 flex-1">
+        <p className="text-lg font-medium">Review my day and plan ahead</p>
+        <div className="flex items-center text-gray-500 text-sm">
+          <p className="mr-2">📅 26 Jul 17:00</p>
+          <p className="mr-2">⏰</p>
+        </div>
+      </div>
+      <div className="flex items-center space-x-4">
+        <div className="text-sm text-gray-500">My work 🎯 / Routines 🔄</div>
+      </div>
     </div>
   );
-}
+};
+
+export default TaskItem;
